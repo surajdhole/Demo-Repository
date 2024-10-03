253954b95169ff55704cb72dd43f3edc03e018e2
Suraj <surajdhole11@gmail.com>
2024-10-03 22:45:13 +0100

    commit for demo project

---
+++ CitiInterview/src/com/java/test/Test.java
@@ -0,0 +1,13 @@
+package com.java.test;
+
+public class Test {
+	int i;
+	float f;
+	char c;
+	double d;
+	public static void main(String[] args) {
+		Integer a = null;
+		//System.out.println(a);
+		System.out.println("Hello from git hub");
+	}
+}
